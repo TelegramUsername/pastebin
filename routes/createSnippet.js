d3ce2daf26a2071de165cb051b13cd6da7f21ed0
TelegramUsername <84450720+TelegramUsername@users.noreply.github.com>
2022-05-10 21:30:07 +0530

    Update createSnippet.js

--- routes/createSnippet.js
+++ routes/createSnippet.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
 
     const content = req.rawText || req.body.snippet;
-    if (!content) return res.redirect('/');
+    if (!content) return res.status(400).json({message: "snippet not found"});
     req.models.snippets.create({content})
         .then(doc => {
             return res.status(201).json({id: `~${doc._id}`});
