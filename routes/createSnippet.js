b471f019f023239f41dcc2172307ba9d5f134b5e
TelegramUsername <84450720+TelegramUsername@users.noreply.github.com>
2022-05-10 21:49:06 +0530

    ..

--- routes/createSnippet.js
+++ routes/createSnippet.js
@@ -1,14 +1,10 @@
 module.exports = (req, res) => {
+
     const content = req.rawText || req.body.snippet;
     if (!content) return res.redirect('/');
-    fetch("/create/", {
-        "method": "POST",
-        "body": JSON.stringify({
-            snippet: content
-        })
-    })
+    req.models.snippets.create({content})
         .then(doc => {
-            return res.redirect(`/~${doc.id}`);
+            res.redirect(`/~${doc._id}`);
         });
 
 };
