a959970758202cf5043ad5d789c0bc5e0091a3dd
TelegramUsername <84450720+TelegramUsername@users.noreply.github.com>
2022-05-10 21:40:30 +0530

    ..

--- routes/createSnippet.js
+++ routes/createSnippet.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
 
     const content = req.rawText || req.body.snippet;
     if (!content) return res.redirect('/');
-    fetch("/create", {
+    fetch("/create/", {
       "method": "POST",
       "body": JSON.stringify({
         snippet: content
